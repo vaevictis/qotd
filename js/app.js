cb2b01acc90303754b179c9cf4c9f5583720de51
Steven Occhipinti <steve.occhipinti@rea-group.com>
2015-04-25 08:35:04 +1000

    Extract a QuoteForm component

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Firebase from 'firebase';
 import ReactFireMixin from 'reactfire';
 import QuoteList from './components/QuoteList.js';
+import QuoteForm from './components/QuoteForm.js';
 require('../css/style.css');
 
 let App = React.createClass({
@@ -11,14 +12,12 @@ let App = React.createClass({
     return { quotes: [] };
   },
 
-  handleSubmit(e) {
-    e.preventDefault();
+  submitQuote(quote, attribution) {
     this.firebaseRefs.quotes.push({
-      quote: this.state.quote,
-      attribution: this.state.attribution,
+      quote: quote,
+      attribution: attribution,
       quoted_at: new Date().getTime()
     });
-    this.setState({attribution: "", quote: ""});
   },
 
   componentWillMount() {
@@ -28,49 +27,11 @@ let App = React.createClass({
     );
   },
 
-  onQuoteChange(e) {
-    this.setState({quote: e.target.value});
-  },
-
-  onAttributionChange(e) {
-    this.setState({attribution: e.target.value});
-  },
-
-  quotes() {
-    return this.state.quotes.map((quote) => {
-      return ( <li>{quote.quote} - {quote.attribution}</li> );
-    });
-  },
-
   render() {
     return (
       <div>
         <h1>Shit My Mates Say</h1>
-
-        <form onSubmit={ this.handleSubmit }>
-          <div className="form-group joined-inputs">
-            <label className="sr-only" htmlFor="quote">New Quote</label>
-            <textarea
-              name="quote"
-              placeholder="Some awesome quote..."
-              onChange={ this.onQuoteChange }
-              value={ this.state.quote }
-              className="form-control"
-              rows="3"
-            /><input
-              name="attribution"
-              placeholder="Who?"
-              onChange={ this.onAttributionChange }
-              value={ this.state.attribution }
-              className="form-control"
-            />
-          </div>
-
-          <div className="form-group">
-            <button className="btn btn-lg btn-default btn-block">{ "Quote it!" }</button>
-          </div>
-        </form>
-
+        <QuoteForm onSubmit={this.submitQuote} />
         <QuoteList quotes={ this.state.quotes } />
       </div>
     );
