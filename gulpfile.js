67eba1f79912db992b80d1364f79924da810f964
Steven Occhipinti <steve.occhipinti@rea-group.com>
2015-04-24 15:57:46 +1000

    Move from npm scripts to gulp tasks for consistency

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,19 @@
 var gulp = require('gulp'),
-  ghPages = require('gulp-gh-pages');
+  ghPages = require('gulp-gh-pages'),
+  shell = require('gulp-shell');
 
-gulp.task('deploy', function() {
+
+gulp.task('default', ['start']);
+
+gulp.task('start', shell.task([
+  "webpack-dev-server --hot --progress --colors"
+]));
+
+gulp.task('build', shell.task([
+  "webpack --progress --colors --config webpack.dist.config.js"
+]));
+
+gulp.task('deploy', ['build'], function() {
   return gulp.src(['index.html', './build/**/*'])
     .pipe(ghPages());
 });
