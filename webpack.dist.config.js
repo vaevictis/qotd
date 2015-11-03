c112b4fec3362b483327e33cdd2d4d7abd1348bb
Guillaume Hammadi <ghammadi@gmail.com>
2015-11-03 17:07:08 +1100

    Add SASS and images compile stage to webpack.dist

--- webpack.dist.config.js
+++ webpack.dist.config.js
@@ -16,6 +16,17 @@ module.exports = {
       {
         test: /\.css$/,
         loader: "style!css"
+      },
+      {
+        test: /\.(scss|sass)$/,
+        loader: "style!css!sass"
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+          'file?hash=sha512&digest=hex&name=[hash].[ext]',
+          'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+        ]
       }
     ]
   },
